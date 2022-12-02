66be4388b2942a1400e3d09d89a3afe0f2fa176b
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-02 02:01:36 +0200

    Fixed issue, the securty configuration was not applying because of missing of bean annotation at filterchain method.

--- src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/configuration/security/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.eacuamba.dev.reddit_clone_using_angular_spring.configuration.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.swing.*;
@@ -10,9 +12,10 @@ import java.awt.event.ActionListener;
 
 @Configuration
 public class SecurityConfiguration {
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
-                //Here I'm authorize all requests to api/auth/**;
+                //Here I authorize all requests to api/auth/**;
                 .authorizeHttpRequests()
                 .requestMatchers("/api/auth/**")
                 .permitAll()
