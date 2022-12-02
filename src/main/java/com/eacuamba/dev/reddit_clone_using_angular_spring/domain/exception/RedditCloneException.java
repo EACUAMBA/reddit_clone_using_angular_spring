92ad9e424e232e501144f4a0d1fb004cd57e0556
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-02 02:03:47 +0200

    Created my own exception to avoid show to the user backend error messages.

---
+++ src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/domain/exception/RedditCloneException.java
@@ -0,0 +1,7 @@
+package com.eacuamba.dev.reddit_clone_using_angular_spring.domain.exception;
+
+public class RedditCloneException extends RuntimeException {
+    public RedditCloneException(String message) {
+        super(message);
+    }
+}
