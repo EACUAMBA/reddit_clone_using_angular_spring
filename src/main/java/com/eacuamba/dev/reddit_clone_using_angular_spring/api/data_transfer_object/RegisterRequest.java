a5ca5178a5afab779fb7492d186da387b7b7165d
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-02 02:04:55 +0200

    Created DTO to hold data send to signup endpoint when register new user.

---
+++ src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/api/data_transfer_object/RegisterRequest.java
@@ -0,0 +1,14 @@
+package com.eacuamba.dev.reddit_clone_using_angular_spring.api.data_transfer_object;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String email;
+    private String username;
+    private String password;
+}
