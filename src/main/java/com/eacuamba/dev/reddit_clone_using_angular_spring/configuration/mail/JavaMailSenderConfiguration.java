1aba1b403befaa884d411a6d0e04c23a0669a81f
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-02 02:08:36 +0200

    Created the configuration for JavaMailSender;

---
+++ src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/configuration/mail/JavaMailSenderConfiguration.java
@@ -0,0 +1,43 @@
+package com.eacuamba.dev.reddit_clone_using_angular_spring.configuration.mail;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class JavaMailSenderConfiguration {
+    private final String mailHost;
+    private final Integer mailPort;
+    private final String protocol;
+    private final String mailUsername;
+    private final String mailPassword;
+
+    public JavaMailSenderConfiguration(
+            @Value("${mail.host}")     String mailHost,
+            @Value("${mail.port}")     Integer mailPort,
+            @Value("${mail.protocol}") String protocol,
+            @Qualifier("mailUsername") String mailUsername,
+            @Qualifier("mailPassword") String mailPassword
+    ) {
+        this.mailHost = mailHost;
+        this.mailPort = mailPort;
+        this.protocol = protocol;
+        this.mailUsername = mailUsername;
+        this.mailPassword = mailPassword;
+    }
+
+
+    @Bean
+    public JavaMailSender javaMailSender(){
+        final JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost(this.mailHost);
+        javaMailSender.setPort(this.mailPort);
+        javaMailSender.setProtocol(this.protocol);
+        javaMailSender.setUsername(this.mailUsername);
+        javaMailSender.setPassword(this.mailPassword);
+        return javaMailSender;
+    }
+}
