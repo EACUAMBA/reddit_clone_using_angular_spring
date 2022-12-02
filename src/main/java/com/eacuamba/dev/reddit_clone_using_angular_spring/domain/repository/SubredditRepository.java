033c06ff993fac787c9a9672aa4e74d0df579e49
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-03 00:02:19 +0200

    Fixed issue with mapping subreddit it creates two tables because i didn't put mapped by property. Addded repository and service.

--- src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/domain/repository/SubredditRepository.java
+++ src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/domain/repository/SubredditRepository.java
@@ -1,7 +1,6 @@
 package com.eacuamba.dev.reddit_clone_using_angular_spring.domain.repository;
 
 import com.eacuamba.dev.reddit_clone_using_angular_spring.domain.model.Subreddit;
-import com.eacuamba.dev.reddit_clone_using_angular_spring.domain.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
