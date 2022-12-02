beeaff8899145981046b63f57da8541dcc8d9c42
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-02 23:43:02 +0200

    Moved some Vote classes to new package, created new VoteService and Repository.

---
+++ src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/domain/model/vote/Vote.java
@@ -0,0 +1,54 @@
+package com.eacuamba.dev.reddit_clone_using_angular_spring.domain.model.vote;
+
+import com.eacuamba.dev.reddit_clone_using_angular_spring.domain.model.Post;
+import com.eacuamba.dev.reddit_clone_using_angular_spring.domain.model.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Table
+@Entity
+public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "vote_type")
+    @Enumerated(EnumType.STRING)
+    private VoteType voteType;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", referencedColumnName = "id")
+    @ToString.Exclude
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @ToString.Exclude
+    private User user;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Vote vote = (Vote) o;
+        return id != null && Objects.equals(id, vote.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/domain/model/vote/VoteType.java
@@ -0,0 +1,13 @@
+package com.eacuamba.dev.reddit_clone_using_angular_spring.domain.model.vote;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum VoteType {
+    UP(1),
+    DOWN(-1);
+
+    private final Integer direction;
+}
