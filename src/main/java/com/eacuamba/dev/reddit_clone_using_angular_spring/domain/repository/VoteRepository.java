30ecc91e0d13abaecbfb1350eb8e109b99b6f247
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-07 23:24:28 +0200

    Added method to retrieve the latest vote by post and user.

--- src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/domain/repository/VoteRepository.java
+++ src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/domain/repository/VoteRepository.java
@@ -1,9 +1,14 @@
 package com.eacuamba.dev.reddit_clone_using_angular_spring.domain.repository;
 
+import com.eacuamba.dev.reddit_clone_using_angular_spring.domain.model.Post;
+import com.eacuamba.dev.reddit_clone_using_angular_spring.domain.model.User;
 import com.eacuamba.dev.reddit_clone_using_angular_spring.domain.model.vote.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findTopByPostAndUserOrderByIdDesc(Post post, User user);
 }
