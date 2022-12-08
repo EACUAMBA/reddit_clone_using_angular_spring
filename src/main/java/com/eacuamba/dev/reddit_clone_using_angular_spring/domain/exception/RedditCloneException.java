4b322808256a668c385a166db834104fc4c85c98
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-08 16:23:54 +0200

    Created some custom exceptions and an exception builder to build some exceptions more quickly.

--- src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/domain/exception/RedditCloneException.java
+++
@@ -1,7 +0,0 @@
-package com.eacuamba.dev.reddit_clone_using_angular_spring.domain.exception;
-
-public class RedditCloneException extends RuntimeException {
-    public RedditCloneException(String message) {
-        super(message);
-    }
-}
