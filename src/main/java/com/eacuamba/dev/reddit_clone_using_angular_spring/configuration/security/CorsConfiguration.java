f29008c52017af2676d2f9389f84028dffc2faec
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-09 17:24:57 +0200

    Configured cors to permit request from our front end.

---
+++ src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/configuration/security/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package com.eacuamba.dev.reddit_clone_using_angular_spring.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfiguration {
+    @Bean(value = "corsConfigurationSource")
+    public CorsConfigurationSource corsConfigurationSource() {
+        org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+        corsConfiguration.setAllowedMethods(List.of("POST", "GET", "PUT", "PATCH", "DELETE"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return urlBasedCorsConfigurationSource;
+    }
+}
