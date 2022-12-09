d2ae0475318ca5ba3237d4d25fcf40a98269d573
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-09 17:22:42 +0200

    Imported some modules for forms and http requests.

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
