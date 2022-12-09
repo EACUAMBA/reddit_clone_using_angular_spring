55da20691d43161bd44e2a90a769300d86d4a46c
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-09 18:07:35 +0200

    Configure spring security to block access to our API endpoints without token.

--- src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/configuration/security/SecurityConfiguration.java
@@ -2,32 +2,52 @@ package com.eacuamba.dev.reddit_clone_using_angular_spring.configuration.securit
 
 import com.eacuamba.dev.reddit_clone_using_angular_spring.configuration.security.filter.JwtTokenValidationFilter;
 import com.eacuamba.dev.reddit_clone_using_angular_spring.configuration.security.filter.JwtUsernameAndPasswordAuthenticationFilter;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
-@RequiredArgsConstructor
 @Configuration
 public class SecurityConfiguration {
     private final JwtUsernameAndPasswordAuthenticationFilter jwtUsernameAndPasswordAuthenticationFilter;
     private final JwtTokenValidationFilter jwtTokenValidationFilter;
+    private final CorsConfigurationSource configurationSource;
+
+    public SecurityConfiguration(
+            JwtUsernameAndPasswordAuthenticationFilter jwtUsernameAndPasswordAuthenticationFilter,
+            JwtTokenValidationFilter jwtTokenValidationFilter,
+            @Qualifier(value = "corsConfigurationSource")
+            CorsConfigurationSource configurationSource) {
+        this.jwtUsernameAndPasswordAuthenticationFilter = jwtUsernameAndPasswordAuthenticationFilter;
+        this.jwtTokenValidationFilter = jwtTokenValidationFilter;
+        this.configurationSource = configurationSource;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
+                .cors()
+                .configurationSource(this.configurationSource)
+                .and()
+
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+
                 .addFilter(this.jwtUsernameAndPasswordAuthenticationFilter)
                 .addFilterAfter(this.jwtTokenValidationFilter, JwtUsernameAndPasswordAuthenticationFilter.class)
 
                 .authorizeHttpRequests()
+
+                .requestMatchers("/api/auth/**")
+                .permitAll()
+
                 .requestMatchers("/api/**")
                 .authenticated()
 
-                .requestMatchers("/api/auth/**", "/**")
+                .requestMatchers("/**")
                 .permitAll();
 
 
