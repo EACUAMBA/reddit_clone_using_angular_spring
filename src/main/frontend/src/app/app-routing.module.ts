662083977ab686808771bcf22103a9062a800803
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-12 14:31:15 +0200

    Added new route to home page.

--- src/main/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SignUpComponent} from "./components/sign-up/sign-up.component";
 import {LoginComponent} from "./components/login/login.component";
+import {HomeComponent} from "./components/home/home.component";
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'sign-up',
     component: SignUpComponent
