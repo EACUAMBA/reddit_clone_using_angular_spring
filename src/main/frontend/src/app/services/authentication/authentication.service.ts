1fdb29bb2f76ac0b1f7aa3d5c91e52642f300fb5
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-15 00:33:03 +0200

    Improved auth service, now i emmit some vales when the user log in and out.

--- src/main/frontend/src/app/services/authentication/authentication.service.ts
+++ src/main/frontend/src/app/services/authentication/authentication.service.ts
@@ -1,8 +1,8 @@
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable, Output} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../../environments/environment.dev";
 import {SignupRequestPayload} from "../../models/signup-request.payload";
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
 import {LoginRequestPayload} from "../../models/login/login-request.payload";
 import {LocalStorageService} from "ngx-webstorage";
 import jwtDecode from "jwt-decode";
@@ -12,10 +12,18 @@ import jwtDecode from "jwt-decode";
 })
 export class AuthenticationService {
 
+  @Output() loggedIn: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() username: EventEmitter<string> = new EventEmitter<string>();
+
   constructor(
     private httpClient: HttpClient,
     private localStorageService: LocalStorageService
   ) {
+
+    if (this.isLoggedIn()) {
+      this.loggedIn.emit(true);
+      this.username.emit(this.getUsername());
+    }
   }
 
   signup(signupRequestPayload: SignupRequestPayload): Observable<any> {
@@ -27,7 +35,14 @@ export class AuthenticationService {
       environment.apiBaseUrl.concat('auth/login'),
       loginRequestPayload,
       {observe: 'response', headers: new HttpHeaders()}
-    );
+    ).pipe(map(data => {
+      this.localStorageService.store("authorization", data.headers.get("authorization"));
+      this.localStorageService.store("refresh-token", data.headers.get("refresh-token"));
+
+      this.loggedIn.emit(true);
+      this.username.emit(this.getUsername());
+      return true;
+    }));
   }
 
   getJWT(): string {
@@ -51,5 +66,23 @@ export class AuthenticationService {
     jwtDecode(this.getJWT())
     let uncodedJwt: any = jwtDecode(this.getJWT());
     return uncodedJwt.sub as string;
+  }
+
+  logout() {
+    this.httpClient.post(environment.apiBaseUrl.concat('auth/logout/'), {refreshToken: this.localStorageService.retrieve('refresh-token')})
+      .subscribe({
+        next: (data) => {
+          console.log(data)
+        },
+        error: (err) => {
+          throw err;
+        }
+      });
+
+    this.localStorageService.clear('authorization');
+    this.localStorageService.clear('refresh-token');
+
+    this.loggedIn.emit(false);
+    this.username.emit('');
   }
 }
