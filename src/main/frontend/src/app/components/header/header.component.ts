57381033a6280700abfbe613115fbb202d7c871a
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-14 22:28:20 +0200

    Improved the header, now have a dropdown to goto user proofile page when user is logged in.

--- src/main/frontend/src/app/components/header/header.component.ts
+++ src/main/frontend/src/app/components/header/header.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "../../services/authentication/authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  isLoggedIn : boolean = false;
-  username? : string = undefined;
+export class HeaderComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  username: string | undefined;
+
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router) {
+
+  }
 
   logout = () => {
     console.log("Want to logout?")
   }
 
-  goToUserProfile = () => {
-    console.log("Want to go to the user profile?")
+  ngOnInit(): void {
+    this.isLoggedIn = this.authenticationService.isLoggedIn();
+    this.username = this.authenticationService.getUsername() as string;
+  }
+
+  goToUserProfile() {
+    this.router.navigateByUrl('/user-profile/' + this.username)
   }
 }
