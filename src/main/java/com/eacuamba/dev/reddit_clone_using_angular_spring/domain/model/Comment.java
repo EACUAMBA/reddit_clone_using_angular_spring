53056bc0bede579317c4500a995610c4b5f7925c
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-14 22:37:50 +0200

    Added timestamp.

--- src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/domain/model/Comment.java
+++ src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/domain/model/Comment.java
@@ -3,7 +3,12 @@ package com.eacuamba.dev.reddit_clone_using_angular_spring.domain.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 
@@ -31,6 +36,11 @@ public class Comment {
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
+    @CreationTimestamp
     @Column(name = "created_at")
     private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
 }
