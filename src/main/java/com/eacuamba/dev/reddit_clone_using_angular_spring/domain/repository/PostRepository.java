7e75cb85161a3014d3c1a0109cea20ca6c12fd63
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-14 22:22:47 +0200

    Added new method to get post by user.

--- src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/domain/repository/PostRepository.java
+++ src/main/java/com/eacuamba/dev/reddit_clone_using_angular_spring/domain/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.eacuamba.dev.reddit_clone_using_angular_spring.domain.repository;
 
 import com.eacuamba.dev.reddit_clone_using_angular_spring.domain.model.Post;
+import com.eacuamba.dev.reddit_clone_using_angular_spring.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByUser(User user);
 }
