6bbb45dceed4e8cc3b22aace0348ef8b4790be2a
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-12-14 22:31:13 +0200

    Imrpoved now you can go to a post se comments.

--- src/main/frontend/src/app/components/post-tile/post-tile.component.ts
+++ src/main/frontend/src/app/components/post-tile/post-tile.component.ts
@@ -1,35 +1,30 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {PostService} from "../../services/post/post.service";
 import {PostResponsePayload} from "../../models/post/post-response.payload";
-import {faArrowDown, faArrowUp, faCircleDot, faComments, faDotCircle} from '@fortawesome/free-solid-svg-icons';
-import {} from '@fortawesome/fontawesome-svg-core';
-import {} from '@fortawesome/angular-fontawesome';
+import {faCircleDot, faComments} from '@fortawesome/free-solid-svg-icons';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-post-tile',
   templateUrl: './post-tile.component.html',
   styleUrls: ['./post-tile.component.css']
 })
-export class PostTileComponent implements OnInit{
-  _posts: Array<PostResponsePayload> = [];
+export class PostTileComponent implements OnInit {
+  @Input() _posts: Array<PostResponsePayload> = [];
   faComments: any = faComments;
   faDotCircle: any = faCircleDot;
 
-  constructor(private postService: PostService) {
+  constructor(private postService: PostService, private router: Router) {
 
   }
 
   goToPost(id: bigint) {
-
+    this.router.navigateByUrl('/view-post/' + id)
   }
 
 
 
   ngOnInit(): void {
-    console.log("Testing ngOnInit")
-    this.postService.getAllPost().subscribe({
-      next: (data)=> this._posts = data
-    })
   }
 
 }
